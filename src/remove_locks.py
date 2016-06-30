8cd6c1397c1c2efca17f15d37a9612914e32512c
Dave Allison <david.allison@theguardian.com>
2016-06-30 16:10:07 +0100

    Adding some code to run an SQL query.

--- src/remove_locks.py
+++ src/remove_locks.py
@@ -4,3 +4,10 @@ import sys
 print "Usage: remove_locks.py user password host port"
 
 conn = psycopg2.connect(database="asset_folder_importer",user=sys.argv[1],password=sys.argv[2],host=sys.argv[3],port=sys.argv[4])
+
+cursor=conn.cursor()
+
+cursor.execute("select * from system where pid=(select pid from system where key='script_version' and value like 'asset_folder_vsingester%' order by timestamp desc limit 1)")
+
+
+
