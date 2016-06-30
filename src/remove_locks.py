6b8f390f6759f4356de2b713e2e94f86e961f5c1
Dave Allison <david.allison@theguardian.com>
2016-06-30 17:39:33 +0100

    Forcing run when not None.

--- src/remove_locks.py
+++ src/remove_locks.py
@@ -13,7 +13,7 @@ cursor.execute("select * from system where key='run_end' and pid=(select pid fro
 for record in cursor:
     print record[0]
 
-if record[3] != None:
+if record[3] == None:
     print "Unlocked"
 
 else:
