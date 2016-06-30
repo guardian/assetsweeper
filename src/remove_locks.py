4c9ff3a88ebf4a6110cfefed21c9d3239dd0df67
Dave Allison <david.allison@theguardian.com>
2016-06-30 17:04:18 +0100

    Attempting to fix SQL.

--- src/remove_locks.py
+++ src/remove_locks.py
@@ -20,4 +20,4 @@ else:
 
     print "Locked"
 
-    cursor.execute("update system set timestamp='{0}' where id={1}").format(time.mktime(),record[0])
+    cursor.execute("update system set timestamp='{0}' where id=4657491").format(time.mktime(),record[0])
