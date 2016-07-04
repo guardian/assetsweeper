07ea848e238df73e5ccdae38276de19d92aa1e6c
Andy Gallagher <andy.gallagher@theguardian.com>
2016-07-04 17:06:13 +0100

    adding in commandline options for configfile and script name.

--- README.md
+++ README.md
@@ -16,15 +16,17 @@ How to remove locks
 
 If vsingester will not run then you can remove the lock my using the remove_locks.py script. To use it become root and run the following command: -
 
-python ./remove_locks.py
+python ./remove_locks.py [--config=/path/to/config/file] [--scriptname=scriptname]
+If --config is not passed, then it expects the config file to be located at /etc/asset_folder_importer.cfg.
+If --scriptname is not passed, then it will work on asset_folder_vsingester.
 
 You should see the following output if there was a lock present when the script was run: -
 
-Locked
+Script is locked. Removing lock.
 
 If the script could not find a lock it will output the following line: -
 
-Not locked
+Script is not locked.
 
 
 Other
--- src/remove_locks.py
+++ src/remove_locks.py
@@ -1,16 +1,25 @@
+#!/usr/bin/env python
 import psycopg2
 import datetime
 from asset_folder_importer.config import configfile
+from optparse import OptionParser
+import re
 
-cfg=configfile("/etc/asset_folder_importer.cfg")
+parser = OptionParser()
+parser.add_option("-c","--config", dest="configfile", help="import configuration from this file",default="/etc/asset_folder_importer.cfg")
+parser.add_option("-s","--scriptname",dest="scriptname", help="break locks for this script", default="asset_folder_vsingester")
+(options, args) = parser.parse_args()
+cfg=configfile(options.configfile)
 
 conn = psycopg2.connect(database="asset_folder_importer",user=cfg.value('database_user'),password=cfg.value('database_password'),host=cfg.value('database_host'),port=cfg.value('database_port'))
 
 cursor=conn.cursor()
 
-cursor.execute("select pid,timestamp from system where key='script_version' and value like 'asset_folder_vsingester%' order by timestamp desc limit 1")
+scriptname = re.sub(r'[\'"\\;]',"",options.scriptname) #remove any nasties to prevent sql injection
+cursor.execute("select pid,timestamp from system where key='script_version' and value like '{0}%' order by timestamp desc limit 1".format(scriptname))
+
 if cursor.rowcount==0:
-    print "No runs found for asset_folder_ingester!"
+    print "No runs found for {0}!".format(scriptname)
     exit(1)
 
 row=cursor.fetchone()
@@ -21,11 +30,11 @@ print "Last run PID was {0} at {1}".format(pid,row[1])
 cursor.execute("select id,key,value,timestamp,pid from system where key='run_end' and pid=%s", (pid,))
 
 if cursor.rowcount==0:
-    print "Locked"
+    print "Script is locked. Removing lock."
 
     cursor.execute("insert into system (key,value,pid) values ('run_end',%s,%s)", (datetime.datetime.now(),str(pid),)
                    )
     conn.commit()
-
+    print "Done."
 else:
-    print "Not locked"
+    print "Script is not locked."
