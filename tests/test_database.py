be4d9ff435167b20f15ba08aebd4fe76791607df
Dave Allison <david.allison@theguardian.com>
2017-01-10 15:31:00 +0000

    Adding a test of the _has_column method. This test checks that the method can find the column id in the system table, the column filepath in the files table, and can not find the column hamster in the files table.

--- tests/test_database.py
+++ tests/test_database.py
@@ -7,3 +7,23 @@ class TestDatabase(unittest.TestCase):
     def __init__(self,*args,**kwargs):
         super(TestDatabase,self).__init__(*args,**kwargs)
         self._cached_id = None
+
+    def test__has_column(self):
+
+        import sys
+        sys.path.insert(0, '../src/')
+
+        from asset_folder_importer.database import importer_db
+        from asset_folder_importer.config import configfile
+
+        cfg=configfile("/etc/asset_folder_importer.cfg")
+
+        __version__ = 'asset_folder_sweeper $Rev$ $LastChangedDate$'
+
+        db = importer_db(__version__,hostname=cfg.value('database_host'),port=cfg.value('database_port'),username=cfg.value('database_user'),password=cfg.value('database_password'))
+
+        self.assertEqual(db._has_column('system','id'),True)
+
+        self.assertEqual(db._has_column('files','filepath'),True)
+
+        self.assertEqual(db._has_column('files','hamster'),False)
