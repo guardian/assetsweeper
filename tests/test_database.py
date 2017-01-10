8c3a447d9c966245a1978f85ca56b9d2a81ad2ea
Dave Allison <david.allison@theguardian.com>
2017-01-10 15:37:22 +0000

    Adding a test of the _has_table method. This test checks that the method can find the system table, the files table, and can not find a table called hamster.

--- tests/test_database.py
+++ tests/test_database.py
@@ -27,3 +27,23 @@ class TestDatabase(unittest.TestCase):
         self.assertEqual(db._has_column('files','filepath'),True)
 
         self.assertEqual(db._has_column('files','hamster'),False)
+
+    def test__has_table(self):
+
+        import sys
+        sys.path.insert(0, '../src/')
+
+        from asset_folder_importer.database import importer_db
+        from asset_folder_importer.config import configfile
+
+        cfg=configfile("/etc/asset_folder_importer.cfg")
+
+        __version__ = 'asset_folder_sweeper $Rev$ $LastChangedDate$'
+
+        db = importer_db(__version__,hostname=cfg.value('database_host'),port=cfg.value('database_port'),username=cfg.value('database_user'),password=cfg.value('database_password'))
+
+        self.assertEqual(db._has_table('system'),True)
+
+        self.assertEqual(db._has_table('files'),True)
+
+        self.assertEqual(db._has_table('hamster'),False)
