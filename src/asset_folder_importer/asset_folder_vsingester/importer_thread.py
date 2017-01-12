b9eb9f0c1185a0079aca7d540a6345705f93a5e1
Andy Gallagher <andy.gallagher@theguardian.com>
2017-01-12 18:40:50 +0000

    getting seperate host/port for pluto

--- src/asset_folder_importer/asset_folder_vsingester/importer_thread.py
+++ src/asset_folder_importer/asset_folder_vsingester/importer_thread.py
@@ -156,7 +156,7 @@ class ImporterThread(threading.Thread):
         
     def ask_pluto_for_projectid(self, filepath):
         from asset_folder_importer.pluto.assetfolder import AssetFolderLocator, ProjectNotFound
-        l = AssetFolderLocator(host=self.cfg.value('vs_host'), port=self.cfg.value('vs_port'),
+        l = AssetFolderLocator(host=self.cfg.value('pluto_host'), port=self.cfg.value('pluto_port'),
                                 user=self.cfg.value('vs_user'), passwd=self.cfg.value('vs_password'))
         
         test_path_segments = os.path.dirname(filepath).split('/')
