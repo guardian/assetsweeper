4e434b4b133a929d705d825a9a8b60bfde0ab9bf
Andy Gallagher <andy.gallagher@theguardian.com>
2017-01-13 10:12:31 +0000

    build fix

--- buildrpm.sh
+++ buildrpm.sh
@@ -32,8 +32,7 @@ echo ----------------------------
 echo Packaging zip
 echo ----------------------------
 
-mkdir assetsweeper
-cp -a src/* assetsweeper
+cd ~
 zip -r assetsweeper-${CIRCLE_BUILD_NUM}.zip assetsweeper/
 aws s3 cp assetsweeper-${CIRCLE_BUILD_NUM}.zip s3://gnm-multimedia-deployables/asset_folder_importer/${CIRCLE_BUILD_NUM}/assetsweeper-${CIRCLE_BUILD_NUM}.zip --acl public-read
 
