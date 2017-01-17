8cff00402c8558b8178837291d6169fb6e284002
Andy Gallagher <andy.gallagher@theguardian.com>
2017-01-17 16:53:58 +0000

    suggested database connection stuff

--- src/asset_folder_importer/tests/test_database.py
+++ src/asset_folder_importer/tests/test_database.py
@@ -2,34 +2,31 @@ __author__ = 'dave'
 
 import unittest
 import logging
+import os
+from asset_folder_importer.database import importer_db
 
 class TestDatabase(unittest.TestCase):
+    dbhost = os.environ['DB_HOST'] if 'DB_HOST' in os.environ else 'localhost'
+    dbport = os.environ['DB_PORT'] if 'DB_PORT' in os.environ else "5432"
+    dbuser = os.environ['DB_USER'] if 'DB_USER' in os.environ else 'asset_folder_importer'
+    dbpass = os.environ['DB_PASS'] if 'DB_PASS' in os.environ else 'assetimporter'
+    dbname = os.environ['DB_NAME'] if 'DB_NAME' in os.environ else 'assetimporter'
+    
     def __init__(self,*args,**kwargs):
         super(TestDatabase,self).__init__(*args,**kwargs)
         self._cached_id = None
 
     def test__has_column(self):
+        __version__ = 'database_test'
 
-        import sys
-        sys.path.insert(0, '../src/')
-
-        from asset_folder_importer.database import importer_db
-        from asset_folder_importer.config import configfile
-
-        cfg=configfile("/etc/asset_folder_importer.cfg")
-
-        __version__ = 'asset_folder_sweeper $Rev$ $LastChangedDate$'
-
-        db = importer_db(__version__,hostname=cfg.value('database_host'),port=cfg.value('database_port'),username=cfg.value('database_user'),password=cfg.value('database_password'))
+        db = importer_db(__version__,hostname=self.dbhost,port=self.dbport,username=self.dbuser,
+                         password=self.dbpass,dbname=self.dbname)
 
         self.assertEqual(db._has_column('system','id'),True)
-
         self.assertEqual(db._has_column('files','filepath'),True)
-
         self.assertEqual(db._has_column('files','hamster'),False)
 
     def test__has_table(self):
-
         import sys
         sys.path.insert(0, '../src/')
 
