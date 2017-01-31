287f4fd49c9b0d37054dfe917475048dbaaea0d6
Dave Allison <david.allison@theguardian.com>
2017-01-31 10:53:45 +0000

    Various changes including a new class and a set of unit tests.

--- src/asset_folder_importer/asset_folder_sweeper/find_files.py
+++ src/asset_folder_importer/asset_folder_sweeper/find_files.py
@@ -3,16 +3,11 @@ from pprint import pprint
 import logging
 import mimetypes
 from asset_folder_importer.asset_folder_sweeper.posix_get_mime import posix_get_mime
-import re
+from asset_folder_importer.asset_folder_sweeper.ignore_list import IgnoreList
 logger = logging.getLogger(__name__)
 
-def re_should_ignore(pathShouldIgnore):
+global_ignore_list = IgnoreList()
 
-    reShouldIgnore = []
-    for expr in pathShouldIgnore:
-        reShouldIgnore.append(re.compile(expr))
-
-    return reShouldIgnore
 
 def check_mime(fullpath,db):
     statinfo = os.stat(fullpath)
@@ -28,28 +23,13 @@ def check_mime(fullpath,db):
 
     return statinfo, mt
 
+
 def find_files(cfg,db):
     #Step three. Find all relevant files and bung 'em in the database
     startpath = cfg.value('start_path',noraise=False)
 
     logger.info("Running from '%s'" % startpath)
 
-    #mark a file as to be ignored, if any of these regexes match. This will prevent vsingester from importing it.
-    pathShouldIgnore = [
-        'Adobe Premiere Pro Preview Files',
-        'System Volume Information',
-        '\.TMP',
-        '\/\.',  #should ignore a literal dot, but only if it follows a /
-        '\.pk$',
-        '\.PFL$',
-        '\.PFR$',   #Cubase creates these filetypes
-        '\.peak$',
-        '_synctemp', #this is created by PluralEyes
-    ]
-
-    reShouldIgnore = re_should_ignore(pathShouldIgnore)
-
-    #try:
     n=0
     for dirpath,dirnames,filenames in os.walk(startpath):
         #print dirpath
@@ -57,28 +37,13 @@ def find_files(cfg,db):
         for name in filenames:
             if name.startswith('.'):
                 continue
-            shouldIgnore = False
 
-            for regex in reShouldIgnore:
-                if regex.search(dirpath) is not None:
-                    shouldIgnore = True
+            shouldIgnore = global_ignore_list.should_ignore(dirpath, name)
 
             fullpath = os.path.join(dirpath,name)
             logger.debug("Attempting to add file at path: '%s'" % fullpath)
             try:
                 statinfo, mt = check_mime(fullpath,db)
-
-                #statinfo = os.stat(fullpath)
-                #pprint(statinfo)
-                #mt = None
-                #try:
-                #    (mt, encoding) = mimetypes.guess_type(fullpath, strict=False)
-                #except Exception as e:
-                #    db.insert_sysparam("warning",e.message)
-                #
-                #if mt is None or mt == 'None':
-                #    mt = posix_get_mime(fullpath,db)
-
                 db.upsert_file_record(dirpath,name,statinfo,mt,ignore=shouldIgnore)
 
             except UnicodeDecodeError as e:
---
+++ src/asset_folder_importer/tests/test_check_mime.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import
+import unittest
+import os
+
+
+class TestCheckMime(unittest.TestCase):
+    dbhost = os.environ['DB_HOST'] if 'DB_HOST' in os.environ else 'localhost'
+    dbport = os.environ['DB_PORT'] if 'DB_PORT' in os.environ else "5432"
+    dbuser = os.environ['DB_USER'] if 'DB_USER' in os.environ else 'asset_folder_importer'
+    dbpass = os.environ['DB_PASS'] if 'DB_PASS' in os.environ else 'assetimporter'
+    dbname = os.environ['DB_NAME'] if 'DB_NAME' in os.environ else 'assetimporter'
+
+    def test_check_mime(self):
+        from asset_folder_importer.asset_folder_sweeper.find_files import check_mime
+        from asset_folder_importer.database import importer_db
+
+        __version__ = 'database_test'
+
+        db = importer_db(__version__,hostname=self.dbhost,port=self.dbport,username=self.dbuser,
+                         password=self.dbpass,dbname=self.dbname)
+
+        #test_list = IgnoreList()
+
+        #self.assertTrue(test_list.should_ignore("/path/to/some/Adobe Premiere Pro Preview Files/subpath","yadayadayada.xxx"))
+
+        filepath = "/etc/hosts"
+
+        self.assertEqual(check_mime(filepath,db),"text/plain")
