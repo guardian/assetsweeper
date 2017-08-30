521aa205b03fd1013af494cdd75a73886e234bdf
Andy Gallagher <andy.gallagher@theguardian.com>
2017-08-30 13:10:41 +0100

    implemented AP lookup

--- src/asset_folder_importer/providers/associated_press.py
+++ src/asset_folder_importer/providers/associated_press.py
@@ -3,7 +3,23 @@ from . import BaseProvider,LookupError
 
 class Provider(BaseProvider):
     def lookup(self,filepath,filename,match_data):
-        # return {'status': 'ok','message': 'it worked!', 'site_id': match_data.group('site_id'),
-        #         'format': match_data.group('format')
-        # }
-        raise LookupError("AP ingest not yet implemented")
+        return {
+            'gnm_asset_category': 'Associated Press',
+            'gnm_asset_restrictions_apply': "AP newswires content. For archive use contact info@aparchive.com",
+            'gnm_mm_provider': 'Associated Press',
+            'gnm_mm_ref': match_data.group('story_id'),
+            'CopyrightandLegalInformation': {
+                'gnm_copyright_legal_copyright': 'Associated Press',
+                'gnm_copyright_legal_provider_clip_id': match_data.group('story_id'),
+            },
+            'RightsProfileInformation': {
+                'ContributorType': {
+                    'gnm_contributor_type_id': 48,
+                    'gnm_contributor_type_name': 'Footage Provider'
+                },
+                'Contributor': {
+                    'gnm_contributor_name': 'The Associated Press Ltd',
+                    'gnm_contributor_id': 'GNL000761'
+                }
+            }
+        }
