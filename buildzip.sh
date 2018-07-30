14a92d598e5c9538b1349064ffe7330f3ebee367
Andy Gallagher <andy.gallagher@theguardian.com>
2018-07-30 21:31:12 +0100

    fix shasum for zipfile

--- buildzip.sh
+++ buildzip.sh
@@ -11,7 +11,7 @@ else
 fi
 
 zip -r assetsweeper-${CIRCLE_BUILD_NUM}.zip assetsweeper/
-HASH=$(${SHASUM} -a 256 "assetsweeper-${CIRCLE_BUILD_NUM}.zip" | cut -d ' ' -f 1)
+HASH=$(${SHASUM} "assetsweeper-${CIRCLE_BUILD_NUM}.zip" | cut -d ' ' -f 1)
 echo -e "sha256=$HASH" > assetsweeper-${CIRCLE_BUILD_NUM}.zip.sha
 aws s3 cp assetsweeper-${CIRCLE_BUILD_NUM}.zip s3://gnm-multimedia-deployables/asset_folder_importer/${CIRCLE_BUILD_NUM}/assetsweeper-${CIRCLE_BUILD_NUM}.zip --acl public-read
 aws s3 cp assetsweeper-${CIRCLE_BUILD_NUM}.zip.sha s3://gnm-multimedia-deployables/asset_folder_importer/${CIRCLE_BUILD_NUM}/assetsweeper-${CIRCLE_BUILD_NUM}.zip.sha --acl public-read
