65c19ca2b6aa3575043bf4d7ea1a9fdaddbcc009
Andy Gallagher <andy.gallagher@theguardian.com>
2018-10-04 15:26:24 +0100

    remember to push the damn image once it's built!

--- build_docker.sh
+++ build_docker.sh
@@ -9,3 +9,5 @@ python ./setup.py sdist --formats=gztar
 cd ${ABSOLUTE_PATH}
 
 docker build . -t andyg42/assetsweeper:${CIRCLE_BUILD_NUM}
+docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
+docker push andyg42/assetsweeper:${CIRCLE_BUILD_NUM}
